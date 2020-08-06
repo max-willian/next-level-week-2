51ae2898285e600b1f2affc7c4e5aab0971a6c17
max_maluf <max.maluf.mazzatech@estadao.com>
2020-08-06 16:40:02 -0300

    criando migrations, e aulas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-    return response.send('continuar a aula em 58min');
-})
+app.use(routes);
 
 app.listen(3333);
