92d34dab52dee672955f7dabd0ac50596d9aa63f
Max W. Maluf <maxwillian63@gmail.com>
2020-08-08 16:59:44 -0300

    pagina de formulario

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './style.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 
 
@@ -11,20 +12,11 @@ function TeacherList(){
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">Time</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Input name="subject" label="Materia" />
+                    
+                    <Input name="week_day" label="Dia da Semana" />
+                    
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
 
